0bd47a3125a4e02bed56f5a83fb001b1b9b06d13
Pratham271 <83640434+Pratham271@users.noreply.github.com>
2021-12-19 21:06:33 +0530

    FirstPrj

---
+++ src/app/Books.ts
@@ -0,0 +1,5 @@
+export const books = [ {title:"Engg. Physics",price:200.00, qty:23},
+                {title:"Engg. Chemistry",price:300.00,qty:10},
+                {title:"Let us C",price:130.00,qty:25},
+                {title:"Operating System",price:400.00,qty:16}
+            ];
